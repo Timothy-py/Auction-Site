7a6622532369ae06fdae55cf7615b3996803c2ea
Timothy-py <adeyeyetimothy33@gmail.com>
2021-01-24 10:11:06 +0100

    setup the app server file and established database connection to mongoDB Atlas

---
+++ routes/auction.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+// const userController = require('../controllers/user')
+
+
+
+// USER ROUTES
+
+// get all users
+// router.get('/users', userController.getAllUsers);
+// add a user
+// router.post('/user/add', userController.addNewUser);
+
+
+module.exports = router;
