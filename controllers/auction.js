8315500f8c3a84e9ad5f5ef237d4ea39d34d539e
Timothy-py <adeyeyetimothy33@gmail.com>
2021-01-26 00:21:20 +0100

    fixing image upload in production

--- controllers/auction.js
+++ controllers/auction.js
@@ -19,17 +19,17 @@ exports.getAllAuctions = (req, res) => {
 // create an auction
 exports.createAuction = (req, res, next) => {
     // get action data from frontend
-
-    // generates a random identifier
-    let file_key = Math.random().toString(12).substring(2,15)
+    console.log(req.file.path)
+    const path = req.file.path;
     var auction = {
         title: req.body.title,
         start_time: req.body.start_time,
         end_time: req.body.end_time,
-        image: {
-            data: fs.readFileSync(path.join('./uploads/' + file_key)),
-            contentType: 'image/png'
-        }
+        // image: {
+        //     data: fs.readFileSync(path.join('./uploads/' + req.body.image.name)),
+        //     contentType: 'image/png'
+        // }
+        image: {path}
     }
     // load/create auction
     Auction.create(auction)
--- middleware/image_upload.js
+++ middleware/image_upload.js
@@ -9,7 +9,7 @@ var storage =  multer.diskStorage({
         })
     },
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, `${file.fieldname}_${+ new Date()}.png`)
     }
 });
 
