db247f99c8dd7537e5ecdddbca15ec19124a81c8
Timothy-py <adeyeyetimothy33@gmail.com>
2022-03-28 12:13:24 +0100

    refactor bid schema :art:

--- models/bid.js
+++ models/bid.js
@@ -7,12 +7,33 @@ const auctionSchema = require('./auction')
 
 // configure bids db model
 const bid = new Schema({
-    bidder: bidderSchema,
-    auction: auctionSchema
+    bidder_name: {
+        type: String,
+        required: true
+    },
+    bidder_email: {
+        type: String,
+        required: true
+    },
+    auction_title: {
+        type: String,
+        required: true
+    },
+    start_time:{
+        type: Date,
+        required: true
+    },
+    end_time:{
+        type: Date,
+        required: true
+    },
+    category:{
+        type: Array,
+        required: true
+    }
 }, {
     timestamps: true
 })
-
 
 const Bid = mongoose.model('Bid', bid)
 
