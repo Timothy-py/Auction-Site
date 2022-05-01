c4230ee1be0798417b84f7d25c2cae34da5186a2
Timothy-py <adeyeyetimothy33@gmail.com>
2022-05-01 15:19:14 +0100

    completed Bidder API testing script :white_check_mark:

--- test/test.js
+++ test/test.js
@@ -16,13 +16,13 @@ chai.should();
 chai.use(chaiHttp);
 // ############################################################################
 
-describe('Bidder Authentication API', () => {
+describe('Bidder API Tests', () => {
 
 // before running the tests, first delete the test user if it exist in the DB
 before(async() => {
     await Bidder.deleteOne({email: 'testuser@gmail.com'})
 })
-
+var TOKEN = ''
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 // Test the Signup route
@@ -61,9 +61,44 @@ describe("Bidder Signin /bidder/signin", ()=>{
         .end((err, res) => {
             res.should.have.status(200);
             res.body.should.have.property('token')
+            TOKEN = res.body.token
         done()
         })
     })
 })
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+// Test list all my auctions route
+describe('Bidder auctions /bidder/myauctions', ()=>{
+
+    it('It should list all the auctions of the logged in bidder', (done) => {
+        chai.request(server)
+        .get('/api/bidder/myauctions')
+        .set('token', TOKEN)
+        .end((err, res) => {
+            res.should.have.status(200)
+            res.body.should.have.property('data')
+        done()
+        })
+    })
 })
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Test list all my bids route
+describe('Bidder bids /bidder/mybids', ()=>{
+    
+    it('It should return all the bids of the logged in bidder', (done) => {
+        chai.request(server)
+        .get('/api/bidder/mybids')
+        .set('token', TOKEN)
+        .end((err, res) => {
+            res.should.have.status(200)
+            res.body.message.should.be.a('string', 'All your bids retrieve successfully')
+        done()
+        })
+    })
+})
+})
+
+
+// ############################################################################
+// describe('Auction API Tests', ())
