14a97dd332b040a191f04d653471b539336b2b23
Timothy-py <adeyeyetimothy33@gmail.com>
2022-05-02 17:20:01 +0100

    setup redis for caching :bento:

--- server.js
+++ server.js
@@ -2,9 +2,16 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 const swaggerDocs = require('./utils/swagger')
 
 require('dotenv').config();
+
+// configure Redis
+const redisUrl = "redis://localhost:6379"
+const client = redis.createClient(redisUrl)
+const redisClient = util.promisify(client.set)
 
 // require routes
 const routes = require('./routes/api');
