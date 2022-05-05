5e1e1d767be61d296c9bf2cb9c102a7171ae5b4f
Timothy-py <adeyeyetimothy33@gmail.com>
2022-05-05 14:32:36 +0100

    fixing cache :wrench:

--- utils/redis.js
+++ utils/redis.js
@@ -5,11 +5,14 @@ const {createClient} = require('redis');
 
 
 const client = createClient(process.env.REDIS_URL);
-client.connect = util.promisify(client.connect);
+// client.connect = util.promisify(client.connect);
 
 // module.exports = async () => {
-client.connect()
-console.log('REDIS Client Connected Successfully')
+client.connect().then((fulfilled)=>{
+    console.log('REDIS Client Connected Successfully')
+}).catch((rejected)=>{
+    console.log('REDIS Client Rejected')
+})
     // return client;
 // }
 
