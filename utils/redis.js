647e8501cfdeb8cf3db46ac91349119916c3d074
Timothy-py <adeyeyetimothy33@gmail.com>
2022-05-05 21:21:37 +0100

    fixed redis client connect :wrench:

--- utils/redis.js
+++ utils/redis.js
@@ -1,20 +1,11 @@
-const util = require('util');
 const {createClient} = require('redis');
-// const client = createClient(process.env.REDIS_URL)
-// client.connect()
 
 
 const client = createClient(process.env.REDIS_URL);
-// client.connect = util.promisify(client.connect);
 
-// module.exports = async () => {
-client.connect().then((fulfilled)=>{
-    console.log('REDIS Client Connected Successfully')
-}).catch((rejected)=>{
-    console.log('REDIS Client Rejected')
-})
-    // return client;
-// }
-
+(async () => {
+    await client.connect();
+    console.log('Redis Client Connected Successfully')
+})();
 
 module.exports = client;
