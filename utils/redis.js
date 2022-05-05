d9991455a284815e0ab3ccc44addad0415d1fa89
Timothy-py <adeyeyetimothy33@gmail.com>
2022-05-05 13:25:30 +0100

    fixing caching...:wrench:

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,17 @@
+const util = require('util');
 const {createClient} = require('redis');
-const client = createClient(process.env.REDIS_URL)
+// const client = createClient(process.env.REDIS_URL)
+// client.connect()
+
+
+const client = createClient(process.env.REDIS_URL);
+client.connect = util.promisify(client.connect);
+
+// module.exports = async () => {
 client.connect()
+console.log('REDIS Client Connected Successfully')
+    // return client;
+// }
+
 
 module.exports = client;
